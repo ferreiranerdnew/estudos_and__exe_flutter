409e960df627df29e50d17535943e62541748d6c
FerreiraNerd <ferreiranerd0@gmail.com>
2023-12-24 16:57:48 -0300

    Autocommit a cada 15 minutos

--- exemplo_2tudo/lib/main.dart
+++ exemplo_2tudo/lib/main.dart
@@ -4,8 +4,128 @@ import 'package:syncfusion_flutter_charts/charts.dart';
 void main() => runApp(_ChartApp());
 
 class _ChartApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
+ @override
+    Widget build(BuildContext context) {
+        final List<ChartData1> histogramData = <ChartData1>[
+        ChartData1(5.250),
+        ChartData1(7.750),
+        ChartData1(0.0),
+        ChartData1(8.275),
+        ChartData1(9.750),
+        ChartData1(7.750),
+        ChartData1(8.275),
+        ChartData1(6.250),
+        ChartData1(5.750),
+        ChartData1(5.250),
+        ChartData1(23.000),
+        ChartData1(26.500),
+        ChartData1(26.500),
+        ChartData1(27.750),
+        ChartData1(25.025),
+        ChartData1(26.500),
+        ChartData1(28.025),
+        ChartData1(29.250),
+        ChartData1(26.750),
+        ChartData1(27.250),
+        ChartData1(26.250),
+        ChartData1(25.250),
+        ChartData1(34.500),
+        ChartData1(25.625),
+        ChartData1(25.500),
+        ChartData1(26.625),
+        ChartData1(36.275),
+        ChartData1(36.250),
+        ChartData1(26.875),
+        ChartData1(40.000),
+        ChartData1(43.000),
+        ChartData1(46.500),
+        ChartData1(47.750),
+        ChartData1(45.025),
+        ChartData1(56.500),
+        ChartData1(56.500),
+        ChartData1(58.025),
+        ChartData1(59.250),
+        ChartData1(56.750),
+        ChartData1(57.250),
+        ChartData1(46.250),
+        ChartData1(55.250),
+        ChartData1(44.500),
+        ChartData1(45.525),
+        ChartData1(55.500),
+        ChartData1(46.625),
+        ChartData1(46.275),
+        ChartData1(56.250),
+        ChartData1(46.875),
+        ChartData1(43.000),
+        ChartData1(46.250),
+        ChartData1(55.250),
+        ChartData1(44.500),
+        ChartData1(45.425),
+        ChartData1(55.500),
+        ChartData1(56.625),
+        ChartData1(46.275),
+        ChartData1(56.250),
+        ChartData1(46.875),
+        ChartData1(43.000),
+        ChartData1(46.250),
+        ChartData1(55.250),
+        ChartData1(44.500),
+        ChartData1(45.425),
+        ChartData1(55.500),
+        ChartData1(46.625),
+        ChartData1(56.275),
+        ChartData1(46.250),
+        ChartData1(56.875),
+        ChartData1(41.000),
+        ChartData1(63.000),
+        ChartData1(66.500),
+        ChartData1(67.750),
+        ChartData1(65.025),
+        ChartData1(66.500),
+        ChartData1(76.500),
+        ChartData1(78.025),
+        ChartData1(79.250),
+        ChartData1(76.750),
+        ChartData1(77.250),
+        ChartData1(66.250),
+        ChartData1(75.250),
+        ChartData1(74.500),
+        ChartData1(65.625),
+        ChartData1(75.500),
+        ChartData1(76.625),
+        ChartData1(76.275),
+        ChartData1(66.250),
+        ChartData1(66.875),
+        ChartData1(80.000),
+        ChartData1(85.250),
+        ChartData1(87.750),
+        ChartData1(89.000),
+        ChartData1(88.275),
+        ChartData1(89.750),
+        ChartData1(97.750),
+        ChartData1(98.275),
+        ChartData1(96.250),
+        ChartData1(95.750),
+        ChartData1(95.250)
+        ];
+
+   final Map<String, int> intervalFrequencies = {};
+
+    for (final data in histogramData) {
+      final interval = (data.value ~/ 10) * 10; // Calcula o intervalo
+      final intervalKey = '$interval-${interval + 10}';
+
+      if (intervalFrequencies.containsKey(intervalKey)) {
+        intervalFrequencies[intervalKey] = intervalFrequencies[intervalKey]! + 1;
+      } else {
+        intervalFrequencies[intervalKey] = 1;
+      }
+    }
+
+    final List<HistogramData> processedData = intervalFrequencies.entries.map((entry) {
+      return HistogramData(entry.key, entry.value);
+    }).toList();
+
     return MaterialApp(
       home: Scaffold(
         body: Center(
@@ -13,12 +133,12 @@ class _ChartApp extends StatelessWidget {
             child: SfCartesianChart(
               series: <ChartSeries>[
                 HistogramSeries<HistogramData, String>(
-                  dataSource: chartData,
-                  yValueMapper: (HistogramData sales, _) => sales.frequency,
-                  binInterval: 20,
+                  dataSource: processedData,
                   showNormalDistributionCurve: true,
                   curveColor: const Color.fromRGBO(192, 108, 132, 1),
-                  borderWidth: 3,
+                  binInterval: 10, // Intervalo de 10 para os dados
+                  xValueMapper: (HistogramData data, _) => data.range,
+                  yValueMapper: (HistogramData data, _) => data.frequency.toDouble(),
                 ),
               ],
             ),
@@ -29,17 +149,15 @@ class _ChartApp extends StatelessWidget {
   }
 }
 
+class ChartData1 {
+  final double value;
+
+  ChartData1(this.value);
+}
+
 class HistogramData {
   final String range;
   final int frequency;
 
   HistogramData(this.range, this.frequency);
 }
-
-List<HistogramData> chartData = [
-  HistogramData('0-20', 7),
-  HistogramData('20-40', 15),
-  HistogramData('40-60', 20),
-  HistogramData('60-80', 10),
-  HistogramData('80-100', 18),
-];
