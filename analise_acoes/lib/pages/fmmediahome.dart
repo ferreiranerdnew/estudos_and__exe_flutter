d248dd36ed8fb642e3198c78a19efb7681157f23
FerreiraNerd <ferreiranerd0@gmail.com>
2023-12-26 09:42:50 -0300

    Autocommit a cada 15 minutos

--- analise_acoes/lib/pages/fmmediahome.dart
+++ analise_acoes/lib/pages/fmmediahome.dart
@@ -287,32 +287,6 @@ class _SceneMediaState extends State<SceneMedia> {
                 ),
               ),
               const SizedBox(height: 200),
-              // RF rodapé da pagina buttom retorno
-              // InkWell(
-              //   onTap: () {
-              //     Navigator.pushReplacement(
-              //       context,
-              //       MaterialPageRoute(
-              //         builder: (context) => TodoListPage(),
-              //       ),
-              //     );
-              //   },
-              //   child: Container(
-              //     padding: const EdgeInsets.fromLTRB(12, 6, 12, 0),
-              //     width: 65,
-              //     height: 53,
-              //     child: Center(
-              //       child: Container(
-              //         width: 53,
-              //         height: 53,
-              //         child: Image.asset(
-              //           'assets/page-1/images/iconhomeazul-1.png',
-              //           fit: BoxFit.cover,
-              //         ),
-              //       ),
-              //     ),
-              //   ),
-              // ),
             ],
           ),
         ),
@@ -413,46 +387,6 @@ class _SceneMediaState extends State<SceneMedia> {
       ),
     );
   }
-
-// // RF GOOGLE ADMOB 22/12/2023
-//   void _createInterstitialAd() {
-//     InterstitialAd.load(
-//         adUnitId: 'ca-app-pub-3940256099942544/1033173712',
-//         request: const AdRequest(),
-//         adLoadCallback: InterstitialAdLoadCallback(
-//           // Called when an ad is successfully received.
-//           onAdLoaded: (InterstitialAd ad) {
-//             // debugPrint('$ad loaded.');
-//             // Keep a reference to the ad so you can show it later.
-//             this._interstitialAd = ad;
-//           },
-//           // Called when an ad request failed.
-//           onAdFailedToLoad: (LoadAdError error) {
-//             debugPrint('InterstitialAd failed to load: $error');
-//           },
-//         ));
-//   }
-
-//   void _showInterstitialAd() {
-//     if (_interstitialAd == null) {
-//       print('Anuncio Null');
-//       return;
-//     }
-//     _interstitialAd?.fullScreenContentCallback = FullScreenContentCallback(
-//       onAdShowedFullScreenContent: (InterstitialAd ad) =>
-//           print('%ad onAdShowedFullScreenContent 2'),
-//       onAdDismissedFullScreenContent: (InterstitialAd ad) {
-//         print('$ad onAdDismissedFullScreenContent. 3 ');
-//         ad.dispose();
-//       },
-//       onAdFailedToShowFullScreenContent: (InterstitialAd ad, AdError error) {
-//         print('$ad onAdFailedToShowFullScreenContent 4: $error');
-//         ad.dispose();
-//       },
-//       onAdImpression: (InterstitialAd ad) => print('$ad impression ocurred 5.'),
-//     );
-//     _interstitialAd!.show();
-//   }
 }
 
 class _ChartData {
--- exemplo_2tudo/lib/main.dart
+++ exemplo_2tudo/lib/main.dart
@@ -168,3 +168,150 @@ class SalesData {
 
   SalesData(this.value);
 }
+
+
+/*
+// função abaixo funciona com grafico apenas modelo a função acima que gera o grafico 
+// d euma forma real tudo que tem nesta função abaico e o modelo para colocar em um buttom
+
+
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:math';
+import 'package:intl/intl.dart';
+
+void main() {
+  runApp(_ChartApp());
+}
+
+class _ChartApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: _MyHomePage(),
+    );
+  }
+}
+
+class _MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<_MyHomePage> {
+  late TooltipBehavior _tooltip;
+  late List<ChartData1> histogramData = [];
+  late List<HistogramData> _histogramData = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchChartData();
+    _tooltip = TooltipBehavior(enable: true);
+  }
+
+  Future<void> fetchChartData() async {
+    // final url = Uri.parse(
+    //     'http://192.168.110.178:8120/acoes?acoes=BBDC3.SA&inicio=2022-01-01');
+    final url = Uri.parse(
+        'http://191.252.200.156:81/acoesboxplot?usuario=appFNB3&senha=SOSlgQOQqlYMXA((i1U2E3909875367****jhbdfb&acoes=PETR4&inicio=2023-11-01');
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final responseData = json.decode(response.body);
+      List<ChartData1> fetchedData = [];
+
+      // responseData.forEach((key, value) {
+      //   List<double> parsedValues =
+      //       List<double>.from(value.map((v) => double.parse(v.toString())));
+      //   fetchedData.add(ChartData1(key, parsedValues));
+      // });
+      responseData.forEach((key, value) {
+        value.forEach((v) {
+          histogramData.add(ChartData1(v));
+        });
+      });
+      print('finalizadfo');
+
+      // setState(() {
+      //   histogramData = fetchedData;
+      // });
+    } else {
+      throw Exception('Failed to load data');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // fetchChartData();
+    final Map<String, int> intervalFrequencies = {};
+
+    for (final data in histogramData) {
+      final interval = (data.value ~/ 10) * 10; // Calcula o intervalo
+      final intervalKey = '$interval-${interval + 10}';
+
+      if (intervalFrequencies.containsKey(intervalKey)) {
+        intervalFrequencies[intervalKey] =
+            intervalFrequencies[intervalKey]! + 1;
+      } else {
+        intervalFrequencies[intervalKey] = 1;
+      }
+    }
+
+    final List<HistogramData> processedData =
+        intervalFrequencies.entries.map((entry) {
+      return HistogramData(entry.key, entry.value);
+    }).toList();
+
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Container(
+            child: SfCartesianChart(
+              series: <ChartSeries>[
+                HistogramSeries<HistogramData, String>(
+                  dataSource: processedData,
+                  showNormalDistributionCurve: true,
+                  curveColor: const Color.fromRGBO(192, 108, 132, 1),
+                  binInterval: 10, // Intervalo de 10 para os dados
+                  // fieldName: 'range', // Mapeamento do eixo x
+                  yValueMapper: (HistogramData data, _) =>
+                      data.frequency.toDouble(),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ChartData1 {
+  final double value;
+
+  ChartData1(this.value);
+}
+// class ChartData1 {
+//   ChartData1(this.value);
+
+//   final String x;
+//   final List<double> y;
+// }
+
+class HistogramData {
+  final String range;
+  final int frequency;
+
+  HistogramData(this.range, this.frequency);
+}
+
+
+
+
+
+
+ */
